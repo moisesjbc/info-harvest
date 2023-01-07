1ed47a7200a49add7223714960d9e5b457aead48
Moisés J <moisesjbc@gmail.com>
2023-01-07 10:15:41 +0000

    Change screen resolution + add keep aspect

--- godot-project/gameplay/data_spawn/data_spawn.gd
+++ godot-project/gameplay/data_spawn/data_spawn.gd
@@ -9,4 +9,3 @@ func _on_spawn_timer_timeout():
 	$data_container.add_child(new_data)
 	new_data.global_position.x = $top_position.global_position.x
 	new_data.global_position.y = rand_range($top_position.global_position.y, $bottom_position.global_position.y)
-	print("foo")

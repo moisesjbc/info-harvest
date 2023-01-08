55794427a939aa28332ce45534ca8ab7951f2d94
Moisés J <moisesjbc@gmail.com>
2023-01-08 00:03:48 +0000

    Implement improvements on data filter

--- godot-project/gameplay/data_filter/data_filter.gd
+++ godot-project/gameplay/data_filter/data_filter.gd
@@ -1,7 +1,8 @@
 extends Sprite
 
 var data_in_area = []
-var analysis_delta = 50.0
+var scan_time = 1.0
+var refutation_speed = 200
 var target_data = null
 var bullet_scene = preload("res://gameplay/bullet/bullet.tscn")
 var active = true
@@ -23,7 +24,7 @@ func _process(delta):
 	if active and len(data_in_area):
 		target_data = data_in_area[0]
 		if not target_data.is_analyzed():
-			target_data.analyze(analysis_delta * delta)
+			target_data.analyze((100 / scan_time) * delta)
 		else:
 			if target_data.is_fake_news():
 				shoot(target_data)
@@ -44,14 +45,29 @@ func _draw():
 func shoot(shoot_target_data):
 	var bullet = bullet_scene.instance()
 	bullet.target_data = shoot_target_data
+	bullet.speed = refutation_speed
 	add_child(bullet)
 	bullet.global_position = global_position
 
 
 func display_influence_area(display_influence_area):
-	$influence_circle.visible = display_influence_area
+	$influence_area/influence_circle.visible = display_influence_area
 
 
 func _on_clickable_area_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
-		$improvement_panel.visible = true
+		$improvement_panel.open()
+
+
+func _on_improvement_panel_scan_time_reduced(scan_time_reduction):
+	scan_time -= scan_time_reduction
+
+
+func _on_improvement_panel_refutation_speed_increased(refutation_speed_increment):
+	refutation_speed += refutation_speed_increment
+
+
+func _on_improvement_panel_scan_area_increased(scan_area_increment):
+	$influence_area.scale.x += scan_area_increment
+	$influence_area.scale.y += scan_area_increment
+	print($influence_area.scale.x)
--- godot-project/gameplay/menus/improvement_panel.gd
+++ godot-project/gameplay/menus/improvement_panel.gd
@@ -1,5 +1,78 @@
 extends Panel
 
+var current_improvement_index = -1
+signal scan_time_reduced
+signal scan_area_increased
+signal refutation_speed_increased
+
+var scan_time_reduction = 0.1
+var scan_time_reduction_cost = 30
+var reduce_scan_time_str = "-" + str(scan_time_reduction) + "s scan time [cost: " + str(scan_time_reduction_cost) + "]"
+
+var scan_area_increment = 0.1
+var scan_area_increment_cost = 30
+var increase_scan_area_str = "+" + str(scan_area_increment * 100) + "% scan area [cost: " + str(scan_area_increment_cost) + "]"
+
+var refutation_speed_increment = 200
+var refutation_speed_increment_cost = 30
+var increase_refutation_time_str = "+" + str(refutation_speed_increment) + " refutation speed [cost: " + str(refutation_speed_increment_cost) + "]"
 
 func _on_close_button_pressed():
 	visible = false
+
+
+func open():
+	visible = true
+	$margin_container/vbox_container/view.visible = true
+	$margin_container/vbox_container/select.visible = false
+	
+	$margin_container/vbox_container/select/reduce_scan_time.text = reduce_scan_time_str
+	$margin_container/vbox_container/select/increase_scan_area.text = increase_scan_area_str
+	$margin_container/vbox_container/select/increment_refutation_speed.text = increase_refutation_time_str
+	
+
+func close():
+	visible = false
+
+
+func select_improvement_index(new_improvement_index):
+	current_improvement_index = new_improvement_index
+	$margin_container/vbox_container/view.visible = false
+	$margin_container/vbox_container/select.visible = true
+
+
+func _on_improvement_0_pressed():
+	select_improvement_index(0)
+
+
+func _on_improvement_1_pressed():
+	select_improvement_index(1)
+
+
+func _on_improvement_2_pressed():
+	select_improvement_index(2)
+
+
+func set_current_improvement_text(text):
+	text.get_slice("[", 0)
+	var current_improvement_button = $margin_container/vbox_container/view.get_child(1 + current_improvement_index) as Button
+	current_improvement_button.text = text
+	current_improvement_button.disabled = true
+
+
+func _on_reduce_scan_time_pressed():
+	set_current_improvement_text(reduce_scan_time_str)
+	emit_signal("scan_time_reduced", scan_time_reduction)
+	close()
+
+
+func _on_increase_scan_area_pressed():
+	set_current_improvement_text(increase_scan_area_str)
+	emit_signal("scan_area_increased", scan_area_increment)
+	close()
+
+
+func _on_increment_refutation_speed_pressed():
+	set_current_improvement_text(increase_refutation_time_str)
+	emit_signal("refutation_speed_increased", refutation_speed_increment)
+	close()

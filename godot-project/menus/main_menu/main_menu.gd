4691f93576ff76086b0fe3661405e58b03fbb34b
Moisés J <moisesjbc@gmail.com>
2023-01-08 14:28:01 +0000

    Add credits menu

--- godot-project/menus/main_menu/main_menu.gd
+++ godot-project/menus/main_menu/main_menu.gd
@@ -3,10 +3,15 @@ extends Control
 
 func _ready():
 	$center_container/panel/margin_container2/vbox_container/buttons_container/exit_button.visible = (OS.get_name() != "HTML5")
+	get_tree().paused = false
 
 
 func _on_play_button_pressed():
 	get_tree().change_scene("res://gameplay/main/main.tscn")
+
+
+func _on_credits_menu_button_pressed():
+	get_tree().change_scene("res://menus/credits_menu/credits_menu.tscn")
 
 
 func _on_exit_button_pressed():
